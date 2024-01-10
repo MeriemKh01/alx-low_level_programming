8242106ec3042a7602996fc5ace8221c91d095b8
MeriemKh01 <MrmKh2001@gmail.com>
2024-01-10 14:58:41 -0800

    advanced

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,39 +1,53 @@
-#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * add_dnodeint_end - adds a new node at the end
- * of a dlistint_t list
+ * main - Generates and prints passwords for the crackme5 executable.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * @head: head of the list
- * @n: value of the element
- * Return: the address of the new element
+ * Return: Always 0.
  */
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	dlistint_t *h;
-	dlistint_t *new;
+	char password[7], *codex;
+	int len = strlen(argv[1]), i, tmp;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
+	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
-	new->n = n;
-	new->next = NULL;
+	tmp = (len ^ 59) & 63;
+	password[0] = codex[tmp];
 
-	h = *head;
+	tmp = 0;
+	for (i = 0; i < len; i++)
+		tmp += argv[1][i];
+	password[1] = codex[(tmp ^ 79) & 63];
 
-	if (h != NULL)
+	tmp = 1;
+	for (i = 0; i < len; i++)
+		tmp *= argv[1][i];
+	password[2] = codex[(tmp ^ 85) & 63];
+
+	tmp = 0;
+	for (i = 0; i < len; i++)
 	{
-		while (h->next != NULL)
-			h = h->next;
-		h->next = new;
+		if (argv[1][i] > tmp)
+			tmp = argv[1][i];
 	}
-	else
-	{
-		*head = new;
-	}
+	srand(tmp ^ 14);
+	password[3] = codex[rand() & 63];
 
-	new->prev = h;
+	tmp = 0;
+	for (i = 0; i < len; i++)
+		tmp += (argv[1][i] * argv[1][i]);
+	password[4] = codex[(tmp ^ 239) & 63];
 
-	return (new);
+	for (i = 0; i < argv[1][0]; i++)
+		tmp = rand();
+	password[5] = codex[(tmp ^ 229) & 63];
+
+	password[6] = '\0';
+	printf("%s", password);
+	return (0);
 }
